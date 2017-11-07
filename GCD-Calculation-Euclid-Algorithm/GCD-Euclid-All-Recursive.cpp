15f197e8091026d77a975c74b99a40b5e1004544
Md Amir Sohail <mdas442006@gmail.com>
2017-11-07 22:13:15 +0530

    GCD of Two Positive Integers (Euclidean Algo), Recursively, in C++ (4)

--- GCD-Calculation-Euclid-Algorithm/GCD-Euclid-All-Recursive.cpp
+++ GCD-Calculation-Euclid-Algorithm/GCD-Euclid-All-Recursive.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Logic : gcd (a, b) = gcd( b%a, a) when a!=0
+/* GCD of two positive integers, recursive*/
 
 int GCD_Euclid_1 ( int min, int max ) // Type 1
 {
@@ -24,15 +25,25 @@ int GCD_Euclid_3 ( unsigned int min, unsigned int max ) // Type 3
 	return (min==0) ? max : GCD_Euclid_3 ( max%min , min ) ;
 }
 
+int GCD_Euclid_4 ( int max, int min ) // Type 4
+{
+	if (min==0)
+		return max; // positive gcd
+		
+	return GCD_Euclid_1 ( min, max%min );
+}
+
 int main()
 {
-	cout << "1. GCD of 12 and 9 is : " << GCD_Euclid_1(1024, 36) << endl ;
+	cout << "1. GCD of 1024 and 36 is : " << GCD_Euclid_1(1024, 36) << endl ;
 	
 	cout << "\n2. GCD of 9 and 12 is : " << GCD_Euclid_2(9, 12) << endl ;
 	cout << "2. GCD of 12 and 9 is : " << GCD_Euclid_2(12, 9) << endl ;
 	
 	cout << "\n3. GCD of 9 and 12 is : " << GCD_Euclid_3(9, 12) << endl ;
 	cout << "3. GCD of 12 and 9 is : " << GCD_Euclid_3(12, 9) << endl ;
+	
+	cout << "\n4. GCD of 102 and 312 is : " << GCD_Euclid_1(102, 312) ;
 	
 	return 0;
 }
@@ -43,11 +54,13 @@ For further details, visit -
 https://en.wikipedia.org/wiki/Euclidean_algorithm
 
 =================== S A M P L E      O U T P U T ===================
-1. GCD of 12 and 9 is : 4
+1. GCD of 1024 and 36 is : 4
 
 2. GCD of 9 and 12 is : 3
 2. GCD of 12 and 9 is : 3
 
 3. GCD of 9 and 12 is : 3
 3. GCD of 12 and 9 is : 3
+
+4. GCD of 102 and 312 is : 6
 *******************************************************************/
